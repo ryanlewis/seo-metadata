e29b3fcb25693f5ed6d442e4b687805eae68b2d8
Ryan Lewis <ryan.lewis@epiphanysolutions.co.uk>
2015-04-13 15:09:23 +0100

    publish.cmd now also pushes to nuget.org

--- publish.cmd
+++ publish.cmd
@@ -4,3 +4,4 @@ FOR /F %%I IN ('DIR pkg\nuget\*.nupkg /b /O:-D') DO set PKG=%%I & GOTO :pushpkg
 :pushpkg
 echo Pushing package %PKG%
 app\Epiphany.SeoMetadata\.nuget\nuget.exe push pkg\nuget\%PKG% -Source https://www.myget.org/F/epiphany/api/v2/package
+app\Epiphany.SeoMetadata\.nuget\nuget.exe push pkg\nuget\%PKG%
