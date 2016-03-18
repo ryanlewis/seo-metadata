6667ed2f28b6797b3391f5ab5c50892968459a45
Ryan Lewis <ryan.lewis@epiphanysolutions.co.uk>
2016-03-18 10:30:30 +0000

    Allow for recursive values (resolves #10)

--- app/Epiphany.SeoMetadata/SeoMetadataPropertyValueConverter.cs
+++ app/Epiphany.SeoMetadata/SeoMetadataPropertyValueConverter.cs
@@ -1,15 +1,28 @@
-﻿using System;
-using Newtonsoft.Json;
+﻿using Newtonsoft.Json;
+using System;
+using System.Configuration;
 using Umbraco.Core.Logging;
 using Umbraco.Core.Models.PublishedContent;
 using Umbraco.Core.PropertyEditors;
 
 namespace Epiphany.SeoMetadata
 {
-    [PropertyValueType(typeof(SeoMetadata))]
+    [PropertyValueType(typeof (SeoMetadata))]
     [PropertyValueCache(PropertyCacheValue.All, PropertyCacheLevel.Content)]
     public class SeoMetadataPropertyValueConverter : PropertyValueConverterBase
     {
+        private const string RecurseAppSettingKey = "SeoMetadata.RecurseIfDefaults";
+        private static readonly bool RecurseIfDefaults;
+
+        static SeoMetadataPropertyValueConverter()
+        {
+            RecurseIfDefaults = false;
+            if (ConfigurationManager.AppSettings[RecurseAppSettingKey] != null)
+            {
+                Boolean.TryParse(ConfigurationManager.AppSettings[RecurseAppSettingKey], out RecurseIfDefaults);
+            }
+        }
+
         public override bool IsConverter(PublishedPropertyType propertyType)
         {
             return propertyType.PropertyEditorAlias != null && propertyType.PropertyEditorAlias.Equals("Epiphany.SeoMetadata");
@@ -23,11 +36,22 @@ namespace Epiphany.SeoMetadata
 
             try
             {
-                return JsonConvert.DeserializeObject<SeoMetadata>(sourceString);
+                var md = JsonConvert.DeserializeObject<SeoMetadata>(sourceString);
+
+                if (RecurseIfDefaults &&
+                    String.IsNullOrWhiteSpace(md.Title) &&
+                    String.IsNullOrWhiteSpace(md.Description) &&
+                    String.IsNullOrWhiteSpace(md.UrlName))
+                {
+                    return null;
+                }
+
+                return md;
             }
             catch (Exception e)
             {
-                LogHelper.Warn<SeoMetadataPropertyValueConverter>(String.Format("Cannot deserialize SeoMetadata - {0} - {1}", e.GetType().Name, e.Message));
+                LogHelper.Warn<SeoMetadataPropertyValueConverter>(String.Format("Cannot deserialize SeoMetadata - {0} - {1}",
+                    e.GetType().Name, e.Message));
                 return null;
             }
         }
