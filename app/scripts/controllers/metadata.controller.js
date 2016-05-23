d5e0a18adf241271f6804bf1164606bbeda7e0a6
Ryan Lewis <ryan.lewis@epiphanysolutions.co.uk>
2016-05-23 14:56:55 +0100

    Refactor of GetUrl() to handle configured URLs better. Improved URL rendering for published and unpublished scenarios

--- app/scripts/controllers/metadata.controller.js
+++ app/scripts/controllers/metadata.controller.js
@@ -1,107 +1,126 @@
-angular.module("umbraco").controller("EpiphanySeoMetadataController", [
-  '$scope', function($scope) {
+angular.module("umbraco")
+    .controller("EpiphanySeoMetadataController",
+    [
+        '$scope', 'contentResource', function($scope, contentResource) {
+            $scope.invalidate = true;
+            $scope.model.hideLabel = true;
+            $scope.serpTitleLength = !!$scope.model.config.serpTitleLength ? $scope.model.config.serpTitleLength : 65;
+            $scope.serpDescriptionLength = !!$scope.model.config.serpDescriptionLength ? $scope.model.config.serpDescriptionLength : 150;
+            $scope.developerName = $scope.model.config.developerName || 'your agency';
+            $scope.doNotIndexExplanation = $scope.model.config.doNotIndexExplanation || '';
 
-    $scope.invalidate = true;
-    $scope.model.hideLabel = true;
-    $scope.serpTitleLength =  !!$scope.model.config.serpTitleLength ? $scope.model.config.serpTitleLength : 65;
-    $scope.serpDescriptionLength = !!$scope.model.config.serpDescriptionLength ? $scope.model.config.serpDescriptionLength : 150;
-    $scope.developerName = $scope.model.config.developerName || 'your agency';
-    $scope.doNotIndexExplanation = $scope.model.config.doNotIndexExplanation || '';
+            // default model.value
+            if (!$scope.model.value) {
+                $scope.model.value = { title: '', description: '', urlName: '', noIndex: false };
+            }
 
-    // default model.value
-    if (!$scope.model.value) {
-      $scope.model.value = { title: '', description: '', urlName: '', noIndex: false };
-    }
+            // a very basic slugify function to replace chars in url
+            function slugify(text) {
+                return text.toString()
+                    .toLowerCase()
+                    .trim()
+                    .replace(/\s+/g, '-')     // Replace spaces with -
+                    .replace(/&/g, '')       // Replace & with nothing
+                    .replace(/[^\w\-]+/g, '') // Remove all non-word chars
+                    .replace(/\-\-+/g, '-');  // Replace multiple - with single -
+            }
 
-    $scope.GetUrl = function() {
+            $scope.init = function() {
+                var content = $scope.GetParentContent();
+                if (!content.published) {
+                    // get the URL of the parent document for later
+                    contentResource.getById(content.parentId).then(function(data) {
+                        $scope.parentUrl = data.urls[0];
+                    });
+                }
+            };
 
-          // regex to text if a URL string is absolute or relative: http://stackoverflow.com/a/10687158
-          var pattern = new RegExp(/^https?:\/\//i);
-          var url = "";
+            $scope.GetUrl = function() {
+                
+                var name, url;
+                var pageContent = $scope.GetParentContent();
+                
+                // handle instances where the document is newly created and unpublished
+                if (!pageContent.published) {
+                    if ($scope.model.value.urlName && $scope.model.value.urlName.length) {
+                        name = slugify($scope.model.value.urlName);
+                    } else if (typeof(pageContent.name) !== 'undefined') {
+                        name = slugify(pageContent.name);
+                    }
 
-          var parentContent = $scope.GetParentContent();
+                    if (typeof(name) === 'undefined' || name.length === 0) {
+                        name = "unpublished-page";
+                    }
 
-          // check if any domain is assigned - use if one exists
-          // note it is only available if the node is published
-          if (parentContent.urls && parentContent.urls.length && parentContent.published) {
-              var nodeUrl = parentContent.urls[0];
+                    return $scope.ProtocolAndHost() + $scope.parentUrl + name + "/";
+                }
 
-              // test if it is an absolute url
-              if (pattern.test(nodeUrl)) {
-                  url = nodeUrl;
-              }
-              else {
-                  url = $scope.ProtocolAndHost() + '/' + nodeUrl;
-              }
+                var nodeUrl = pageContent.urls[0];
 
-              if ($scope.model.value.urlName && $scope.model.value.urlName.length) {
-                  var urlSplit = url.split('/');
+                // test if it is an absolute url: http://stackoverflow.com/a/10687158
+                url = /^https?:\/\//i.test(nodeUrl) ? nodeUrl : $scope.ProtocolAndHost() + nodeUrl;
 
-                  // http://mydomain.com will be splitted into an array of 3 strings: 'http:', '' and 'mydomain.com'
-                  // only handle slugify of urlName under root level
-                  if (urlSplit.length > 4) {
-                      if (urlSplit[urlSplit.length - 1] == "") {
-                          urlSplit[urlSplit.length - 2] = slugify($scope.model.value.urlName);
-                      } else {
-                          urlSplit[urlSplit.length - 1] = slugify($scope.model.value.urlName);
-                      }
-                  }
+                var urlSplit = url.split('/');
 
-                  // join new values
-                  url = urlSplit.join('/');
-              }
-          }
-          else if ($scope.model.value.urlName && $scope.model.value.urlName.length) {
-              if (!parentContent.published) {
-                  url = $scope.ProtocolAndHost() + '/unpublished-page/';
-              } else {
-                  url = $scope.ProtocolAndHost() + '/' + slugify($scope.model.value.urlName) + '/';
-              }
-          }
+                // we got a slug configured?
+                if ($scope.model.value.urlName && $scope.model.value.urlName.length) {
 
-          return url;
-    };
+                    // http://mydomain.com will be split into an array of 3 strings: 'http:', '' and 'mydomain.com'
+                    // only handle slugify of urlName under root level
+                    if (urlSplit.length > 4) {
+                        if (urlSplit[urlSplit.length - 1] === "") {
+                            urlSplit[urlSplit.length - 2] = slugify($scope.model.value.urlName);
+                        } else {
+                            urlSplit[urlSplit.length - 1] = slugify($scope.model.value.urlName);
+                        }
+                    }
 
-    $scope.ProtocolAndHost = function() {
+                    // join new values
+                    url = urlSplit.join('/');
+                } else {
+                    // use the name of the document instead
+                    urlSplit.splice(-2, 2); // pop the last slug off the end of the array, use the document name instead
+                    name = pageContent.name ? slugify(pageContent.name) : 'unpublished-page';
+                    url = urlSplit.join('/') + '/' + name + '/';
+                }
 
-      var http = location.protocol;
-      var slashes = http.concat("//");
-      return slashes.concat(window.location.hostname);
+                return url;
+            };
 
-    };
+            $scope.ProtocolAndHost = function() {
 
-    $scope.GetParentContent = function() {
-      var currentScope = $scope.$parent;
+                var http = location.protocol;
+                var slashes = http.concat("//");
+                return slashes.concat(window.location.hostname);
 
-      for (var i = 0; i < 150; i++) {
-        if (currentScope.content) {
-          return currentScope.content;
+            };
+
+            $scope.GetParentContent = function() {
+                var currentScope = $scope.$parent;
+
+                for (var i = 0; i < 150; i++) {
+                    if (currentScope.content) {
+                        return currentScope.content;
+                    }
+
+                    currentScope = currentScope.$parent;
+                }
+
+                return null;
+            };
+
+
+            $(window).resize(function() {
+                $scope.$apply(function() {
+                    if (window.innerWidth <= 1500 && !$scope.hideSerp) {
+                        $scope.hideSerp = true;
+                    }
+                    if (window.innerWidth > 1500) {
+                        $scope.hideSerp = false;
+                    }
+                });
+            });
+
+            $scope.init();
         }
-
-        currentScope = currentScope.$parent;
-      }
-
-      return null;
-    };
-
-
-    $(window).resize(function() {
-      $scope.$apply(function() {
-        if (window.innerWidth <= 1500 && !$scope.hideSerp) {
-        	$scope.hideSerp = true;
-        }
-        if (window.innerWidth > 1500) {
-        	$scope.hideSerp = false;
-        }
-      });
-    });
-      // a very basic slugify function to replace chars in url
-      function slugify(text) {
-          return text.toString().toLowerCase().trim()
-            .replace(/\s+/g, '-')           // Replace spaces with -
-            .replace(/&/g, '-')             // Replace & with -
-            .replace(/[^\w\-]+/g, '')       // Remove all non-word chars
-            .replace(/\-\-+/g, '-')         // Replace multiple - with single -
-      }
-  }
-]);
+    ]);
